2d4930bda3c68262baddc8371c91aad162a084ac
sammy-boy <sammyorondo2@gmail.com>
2024-06-06 10:09:32 +0300

    feat: add profile model and signals

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. //abstract model
+
+import uuid
+
+class TimeStampedUUIDModel(models.Model):
+    pkid=models.BigAutoField(primary_key=True, editable=False)
+    id=models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class Meta: 
+    abstract = True
--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -116,7 +116,7 @@ STATIC_URL = "/staticfiles/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIR = []
 MEDIA_URL = "/mediafiles/"
-MEDIA_ROOT = BASE_DIR / "/mediafiles/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
